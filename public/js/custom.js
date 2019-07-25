327179ad9316e25da432824e24bb6fa0ebc58241
unknown <ghozam.alghifari96@gmail.com>
2019-07-25 16:50:22 +0700

    tampilan

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,23 +1,63 @@
-$(document).ready(function () {
-// confirm delete
-$(document.body).on('submit', '.js-confirm', function () {
-var $el = $(this)
-var text = $el.data('confirm') ? $el.data('confirm') : 'Anda yakin melakukan tindakan ini\
-?'
-var c = confirm(text);
-return c;
+
+$('.js-selectize-reguler').selectize({
+	sortField: 'text'
 });
 
-
-// add selectize to select element
-$('.js-selectize').selectize({
-sortField: 'text'
+$('.js-selectize-multi').selectize({
+	sortField: 'text',
+	delimiter: ',',
+	maxItems: null,
 });
-
 
 $('.datepicker').datepicker({
-
-	autoclose: true
+	format: 'yyyy-mm-dd', 
+	autoclose: true,
 });
 
-});
+Number.prototype.format = function(n, x, s, c) {
+	var re = '\\d(?=(\\d{' + (x || 3) + '})+' + (n > 0 ? '\\D' : '$') + ')',
+	num = this.toFixed(Math.max(0, ~~n));
+
+	return (c ? num.replace('.', c) : num).replace(new RegExp(re, 'g'), '$&' + (s || ','));
+};
+
+//TAMBAH TITIK
+function tandaPemisahTitik(b){
+	var _minus = false;
+	if (b<0) _minus = true;
+	b = b.toString();
+	b=b.replace(".","");
+	b=b.replace("-","");
+	c = "";
+	panjang = b.length;
+	j = 0;
+	for (i = panjang; i > 0; i--){
+		j = j + 1;
+		if (((j % 3) == 1) && (j != 1)){
+			c = b.substr(i-1,1) + "." + c;
+		} else {
+			c = b.substr(i-1,1) + c;
+		}
+	}
+	if (_minus) c = "-" + c ;
+	return c;
+}
+
+//HAPUS TITIK
+function bersihPemisah(ini){
+	a = ini.toString().replace(".","");
+	//a = a.replace(".","");
+	return a;
+}
+
+//Titile Case
+function titleCase(str) { 
+	var newstr = str.split(" "); 
+	for(i=0;i<newstr.length;i++){ 
+		if(newstr[i] == "") continue; 
+		var copy = newstr[i].substring(1).toLowerCase(); 
+		newstr[i] = newstr[i][0].toUpperCase() + copy; 
+	} 
+	newstr = newstr.join(" "); 
+	return newstr; 
+}  
